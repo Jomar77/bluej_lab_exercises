b9961f7d01e2aa7638944bec9adb04c9dbf62e0d
JOMAR NACORDA <jnacorda04@gmail.com>
2023-02-27 21:23:14 +1300

    9 -10

---
+++ exercise10.java
@@ -0,0 +1,18 @@
+/*
+Read, from standard input a single integer
+Write out the times table of that integer from 1 times the number to 10 times the
+number and formatted as shown below.*/
+
+import java.util.Scanner;
+
+public class exercise10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+        System.out.println("Enter a number: ");
+        number = input.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " times " + i + " is " + number * i);
+        }
+    }
+}
--- exercise9.java
+++ exercise9.java
@@ -5,3 +5,24 @@ Output on a line Sum: followed by the sum of the series of integers as shown bel
 Output on a new line Average: followed by the mean average of the series of
 numbers formatted to a precision of 2 decimal places as shown below.
 */
+
+import java.util.Scanner;
+
+public class exercise9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int score;
+        int sum = 0;
+        int count = 0;
+        System.out.println("Enter a score: ");
+        score = input.nextInt();
+        while (score != 999) {
+            sum += score;
+            count++;
+            System.out.println("Enter a score: ");
+            score = input.nextInt();
+        }
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + String.format("%.2f", (double) sum / count));
+    }
+}
