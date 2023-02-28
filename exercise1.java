07b4a851ecca6aba1fb2e6628706e24d79b53c1d
JOMAR NACORDA <jnacorda04@gmail.com>
2023-03-01 09:39:52 +1300

    I dont know how

--- exercise1.java
+++ exercise1.java
@@ -4,13 +4,21 @@ Write a greeting to that person in the form
 Hello first-name family-name of location
 (use single spaces for separators)*/
 
+/* 
+ * Author  : @Jomar77
+ * Version : @2020-10-05
+ */
+
 import java.util.Scanner;
 
 public class exercise1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter your First Name:");
         String firstName = input.nextLine();
+        System.out.println("Enter your Family Name:");
         String familyName = input.nextLine();
+        System.out.println("Enter your Location:");
         String location = input.nextLine();
         System.out.println("Hello " + firstName + " " + familyName + " of " + location);
     }
---
+++ exercise12.java
@@ -0,0 +1,13 @@
+/*
+Write a program that reads in a series of lines terminated by a line
+containing only a single '#'. By counting the spaces, output the number of words on each line
+Do not output a result for the line containing the #
+(Hint Use for – counter controlled loop)
+ */
+
+import java.util.Scanner;
+
+
+
+
+
