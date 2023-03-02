4894d5a5a858cffec0dcbe91d89523be442e103b
JOMAR NACORDA <jnacorda04@gmail.com>
2023-03-02 21:02:08 +1300

    sdfasd

--- exercise22.java
+++ exercise22.java
@@ -1,28 +1,42 @@
+/*Write a program that reads in a series of lines terminated by a line
+containing only a single '#'
+Each line contains names and a series of test scores in the format
+Surname,Firstname, score1, score2, score3, score4 etc
+You are to output results in the format Firstname Surname TotalScore
+There could be any number of scores.
+If there are no scores the total is of course 0
+Do not output a result for the line containing the #
+(Hint Use hasNextInt)
+*/
+
 import java.util.Scanner;
 
 public class exercise22 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int a = 0;
-        String[] words = new String[0];
-        System.out.println("Enter a line of text terminated by a #");
-        String line = input.nextLine();
-        while (!line.equals("#")) {
-            Scanner lineScanner = new Scanner(line);
-            words = line.split(",");
-            while (lineScanner.hasNext()) {
-                if (lineScanner.hasNextInt()) {
-                    for (int j = 2; j < words.length; j++) {
-                        a = a + lineScanner.nextInt();
-                    }
-                } else {
-                    lineScanner.next();
-                }
+        Scanner scan = new Scanner(System.in);
+        String line = "";
+        String firstname = "";
+        String surname = "";
+        int total = 0;
+        int score = 0;
+
+        System.out.print("Enter a line of text terminated by a # : ");
+        line = scan.nextLine();
+        while(!line.equals("#"))
+        {
+            Scanner scanLine = new Scanner(line);
+            scanLine.useDelimiter(",");
+            surname = scanLine.next();
+            firstname = scanLine.next();
+            while(scanLine.hasNextInt())
+            {
+                score = scanLine.nextInt();
+                total = total + score;
             }
-            System.out.printf("%s %s %d", words[1], words[0], a);
-            a = 0;
-            System.out.println("Enter a line of text terminated by a #");
-            line = input.nextLine();
+            System.out.println(firstname + " " + surname + " " + total);
+            total = 0;
+            System.out.print("Enter a line of text terminated by a # : ");
+            line = scan.nextLine();
         }
     }
 }
