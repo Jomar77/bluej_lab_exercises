0e5c78601f50b3f83a904c85b9cccce1b99891e6
JOMAR NACORDA <jnacorda04@gmail.com>
2023-03-04 21:50:27 +1300

    30

---
+++ exercise30.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class exercise30 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        
+        System.out.println("Enter your numbers:");
+        String number = input.nextLine();
+        while (!number.equals("#")) {
+            Scanner lineScanner = new Scanner(number);
+            while (lineScanner.hasNext()) {
+                    numbers.add(lineScanner.nextInt());
+            }
+            for (int i = 0; i < numbers.size(); i++) {
+                for (int j = 0; j < numbers.size() - 1; j++) {
+                    if (numbers.get(j) > numbers.get(j + 1)) {
+                        int temp = numbers.get(j);
+                        numbers.set(j, numbers.get(j + 1));
+                        numbers.set(j + 1, temp);
+                    }
+                }
+            }
+            for (int i : numbers) {
+                System.out.print(i + " ");
+            }
+            
+            numbers = new ArrayList<Integer>();
+            System.out.println("\nEnter your numbers:");
+            number = input.nextLine();
+        }
+    }
+}
