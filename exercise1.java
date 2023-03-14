14b6049fda3b19e50b4188981114ee432c0a1871
JOMAR NACORDA <jnacorda04@gmail.com>
2023-03-15 11:00:11 +1300

    1

--- exercise1.java
+++ exercise1.java
@@ -1,24 +1,10 @@
-/*Read, from standard input, a person’s first name, family name and their home location
-– each on a separate line
-Write a greeting to that person in the form
-Hello first-name family-name of location
-(use single spaces for separators)*/
-
-/* 
- * Author  : @Jomar77
- * Version : @2020-10-05
- */
-
 import java.util.Scanner;
 
 public class exercise1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter your First Name:");
         String firstName = input.nextLine();
-        System.out.println("Enter your Family Name:");
         String familyName = input.nextLine();
-        System.out.println("Enter your Location:");
         String location = input.nextLine();
         System.out.println("Hello " + firstName + " " + familyName + " of " + location);
     }
