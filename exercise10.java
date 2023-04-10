011ac2a7cbff668801a852dcb0181889613f37c1
JOMAR NACORDA <jnacorda04@gmail.com>
2023-04-10 21:21:48 +1200

    exercise somethihng

--- exercise10.java
+++ exercise10.java
@@ -4,9 +4,9 @@ public class exercise10 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
-        printTable(number);
+        exercise10.printTable(number);
     }
-    public static void printTable(int number){
+    static void printTable(int number){
         for (int i = 1; i <= 10; i++) {
             System.out.println(i + " times " + number + " is " + number * i);
         }
