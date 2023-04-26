94358021e043ded8bc95c27772b94cff4e42b8c9
JOMAR NACORDA <jnacorda04@gmail.com>
2023-04-26 21:23:45 +1200

    exe

--- exercise19.java
+++ exercise19.java
@@ -1,23 +1,31 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class exercise19 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String name = "";
-        int temp = 0;
         String tempName = "";
         String line = input.nextLine();
 
         while (!line.equals("#")) {
             String[] parts = line.split(" ");
-            parts.sort()
-            for (int i = 0; i < sortedScores.length; i++) {
-                System.out.print(sortedNames[i] + " " + sortedScores[i]);
-                if (i != sortedScores.length - 1) {
-                    System.out.print(" ");
+            int [] num = new int[parts.length];
+            num[0] = Integer.parseInt(parts[1]);
+            num[1] = Integer.parseInt(parts[3]);
+
+                if (num[1] > num[0]) {
+                    tempName = parts[3];
+                    parts[3] = parts[1];
+                    parts[1] = tempName;
+                    tempName = parts[0];
+                    parts[0] = parts[2];
+                    parts[2] = tempName;
                 }
+                
+            for (int i = 0; i < parts.length; i++){
+                System.out.print(parts[i] + " ");
             }
             System.out.println();
             line = input.nextLine();
         }
+    }
+}
