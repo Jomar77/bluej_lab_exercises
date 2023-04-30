9911422813ca4936fa9eac61d1686986f6e929c1
JOMAR NACORDA <jnacorda04@gmail.com>
2023-04-30 18:17:51 +1200

    28

--- exercise28.java
+++ exercise28.java
@@ -7,13 +7,19 @@ public class exercise28 {
         Scanner input = new Scanner(System.in);
         ArrayList<Integer> numbers = new ArrayList<Integer>();
         int number = input.nextInt();
-        while (number != -1) {
-            numbers.add(number);
-            number = input.nextInt();
-        }
+        concat(number, numbers);
+        
         Collections.sort(numbers);
         for (int i : numbers) {
             System.out.println(i);
+        }
+    }
+
+    public static void concat(int number, ArrayList<Integer> numbers){
+        Scanner input = new Scanner(System.in);
+        while (number != -1) {
+            numbers.add(number);
+            number = input.nextInt();
         }
     }
 }
