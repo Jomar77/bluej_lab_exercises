b969ea922e6b421071db052e4d405fd49609e6e7
JOMAR NACORDA <jnacorda04@gmail.com>
2023-04-30 21:15:40 +1200

    e1

--- e13.java
+++ e13.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class e13 {
-    
+    public static void main (String args []){
+
+        Scanner s = new Scanner(System.in);
+        String line = s.nextLine();
+
+        while(!line.equals("#")){
+            for(int i = line.length(); i > 0; i-- ){
+                System.out.print(line.charAt(i-1));
+            }
+            System.out.println();
+            line = s.nextLine();
+        }
+    }
 }
