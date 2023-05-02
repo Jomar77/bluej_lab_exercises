97ce551d3d934cf9317fd1bbf9913f1f0564f8b0
JOMAR NACORDA <jnacorda04@gmail.com>
2023-05-03 08:56:01 +1200

    c

--- exercise12.java
+++ exercise12.java
@@ -2,23 +2,28 @@ import java.util.Scanner;
 
 public class exercise12 {
     public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)) {
+            Scanner input = new Scanner(System.in);
             int count = 1;
             String line = input.nextLine();
             while (!line.equals("#")) {
+                
                 System.out.println(count(line,count));
                 count = 1;
                 line = input.nextLine();
             }
-        }
+        
     }
 
     public static int count(String line, int counter) {
-        for (char c : line.toCharArray()) {
-            if (c == ' ') {
+        if(line.equals("")){
+            counter =0;
+        }else {
+        for (int i =0; i <line.length(); i++) {
+            if (line.charAt(i) == ' ') {
                 counter++;
             }
         }
+    }
         return counter;
     }
 }
