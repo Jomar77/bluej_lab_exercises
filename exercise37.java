d60adc22882d2cc741ebf9a5d3f741b6fb019c73
JOMAR NACORDA <jnacorda04@gmail.com>
2023-05-07 20:05:56 +1200

    37

--- exercise37.java
+++ exercise37.java
@@ -1,28 +1,3 @@
-/*
- * The file numberplates.txt contains a list of number plates and the
-registered car owner for that number plate.
-Each line in the file contains a number plate (a string without spaces), a
-colon : and a name (a string which can have spaces)
-Read the list and create a HashMap using the number plate as the key and
-the name as the data
-Sample file numberplates.txt
-LZ3234:Harrison Ford
-AA5323:Helen Mirren
-ZA4334:Sarah Anne Smith
-HG3245:Joe Brown
-From standard input read a list of number plates
-For each line output the registered owner for that number plate or
-"Unregistered Car" if the number plate is not found.
-The input data will end in a single line containing a #
-Do not process the line with the # in it
-Sample Input Sample Output
-HG3245 Joe Brown
-SD2234 Unregistered Car
-ZA4334 Sarah Anne Smith
-#
- */
-
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -43,7 +18,6 @@ public class exercise37 {
                 line = in.readLine();
             }
             Scanner keyboard = new Scanner(System.in);
-            System.out.println("Put numberplate:");
             line = keyboard.nextLine();
             while (!line.equals("#")) {
                 if (numberplates.containsKey(line)) {
