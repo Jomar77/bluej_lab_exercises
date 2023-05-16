b8c0453743c0656cfae0b1f76782ea05b82262e8
JOMAR NACORDA <jnacorda04@gmail.com>
2023-05-16 13:30:29 +1200

    e

--- exercise25.java
+++ exercise25.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class exercise25 {
     public static void main(String[] args) {
-        try{
+
         Scanner input = new Scanner(System.in);
         int len = 0;
         String [] words = new String[0];
@@ -15,9 +15,7 @@ public class exercise25 {
         for (int i = 0; i < len; i++) {
             System.out.println(words[i]);
         }
-    }catch(Exception e){
-        System.out.println(e);
-    }
+
 }
 
     public static void input(String[] words, int length) {
