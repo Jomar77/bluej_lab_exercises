6a55e0a1c767cc7f327d7adc830206b8fe185e59
JOMAR NACORDA <jnacorda04@gmail.com>
2023-05-22 11:26:19 +1200

    e

--- exercise26.java
+++ exercise26.java
@@ -1,23 +1,52 @@
+/*
+ * 
+ * In order to make the internet more user-friendly we can filter out
+unpleasant words and replace them with more pleasant ones
+Write a program that is supplied a series of comma delimited pairs of
+words as command line parameters and then proceeds to read in lines
+from standard input until a line is entered containing a single '#'.
+For each line except for the terminating line output the same line with the
+unpleasant words replaced by the pleasant words
+For this prototype you can assume the input will
+all be lower case and have no punctuation marks.
+The sample input should produce the given output
+when executing Lab26 as shown to the right
+
+Sample Input 
+todays exam will be difficult 
+you need to work harder 
+the assessment is due now 
+#
+
+
+Sample Output
+todays quiz will be easy
+you need to play harder
+the assessment is due later
+
+
+(Hint Use the args array and the String replace method)
+ */
+
 import java.util.Scanner;
 
-public class exercise26 {
-    public static void main(String[] args) {
-        String[] unpleasantWords = new String[args.length / 2];
-        String[] pleasantWords = new String[args.length / 2];
-        for (int i = 0; i < args.length; i += 2) {
-            String[] pair = args[i].split(",");
-            unpleasantWords[i / 2] = pair[0];
-            pleasantWords[i / 2] = pair[1];
-        }
-
-        Scanner input = new Scanner(System.in);
-        String line =    input.nextLine();
-        while (!line.equals("#")) {
-            for (int i = 0; i < unpleasantWords.length; i += 2) {
-                line = line.replace(unpleasantWords[i], pleasantWords[i]);
-            }
-            System.out.println(line);
-            line = input.nextLine();
-        }
+public class exercise26{
+  public static void main(String[] args){
+    args = new String[]{"difficult,easy", "work,play", "now,later"};
+    String[] words = new String[args.length*2];
+    Scanner scan = new Scanner(System.in);
+    for(int i = 0; i < args.length; i++){
+        words[i] = args[i].split(",")[0];
+        words[i+1] = args[i].split(",")[1];
+        System.out.print(words[i] + " " + words[i+1] + " ");
     }
+    String line = scan.nextLine();
+    while(!line.equals("#")){
+      for(int i = 0; i < args.length; i ++){
+        line = line.replace(words[i], words[i+1]);
+      }
+      System.out.println(line);
+      line = scan.nextLine();
+    }
+  }
 }
--- exercise32.java
+++ exercise32.java
@@ -1,21 +1,3 @@
-/*
- * Write a program that reads in a series of lines from file raw.txt processes
-them and writes them out to standard output
-Each line contains a name and a series of scores for a game - all data on
-the line is separated by spaces
-Output the name and the top three scores in descending order
-There will always be at least 3 scores for each name in the raw data file
-
-Sample File raw.txt 
-Mary 45 87 23 76 
-Joe 34 76 12 78 34 87 
-Anne 90 5 99 
-
-Sample Output
-Mary 87 76 45
-Joe 87 78 76
-Anne 99 90 5
- */
 import java.util.Scanner;
 import java.util.Arrays;
 import java.io.File;
