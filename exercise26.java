125d1ae9f9bc060a5d7bee3a10d351acc36a3d42
JOMAR NACORDA <jnacorda04@gmail.com>
2023-05-21 20:36:34 +1200

    e

--- exercise26.java
+++ exercise26.java
@@ -2,13 +2,19 @@ import java.util.Scanner;
 
 public class exercise26 {
     public static void main(String[] args) {
-        String[] replaceWords = args;
+        String[] unpleasantWords = new String[args.length / 2];
+        String[] pleasantWords = new String[args.length / 2];
+        for (int i = 0; i < args.length; i += 2) {
+            String[] pair = args[i].split(",");
+            unpleasantWords[i / 2] = pair[0];
+            pleasantWords[i / 2] = pair[1];
+        }
 
         Scanner input = new Scanner(System.in);
         String line =    input.nextLine();
         while (!line.equals("#")) {
-            for (int i = 0; i < replaceWords.length; i += 2) {
-                line = line.replace(replaceWords[i], replaceWords[i + 1]);
+            for (int i = 0; i < unpleasantWords.length; i += 2) {
+                line = line.replace(unpleasantWords[i], pleasantWords[i]);
             }
             System.out.println(line);
             line = input.nextLine();
