880c142112814df229e5582c8c319c3f9e971f11
JOMAR NACORDA <jnacorda04@gmail.com>
2023-05-22 16:34:16 +1200

    e

--- exercise28.java
+++ exercise28.java
@@ -1,29 +1,21 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class exercise28{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] numbers = new int[100];
-        int i = 0;
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
         int input = 0;
         while(input != -1){
             input = sc.nextInt();
             if(input != -1){
-                numbers[i] = input;
-                i++;
+                numbers.add(input);
             }
         }
-        for(int j = 0; j < i; j++){
-            for(int k = 0; k < i; k++){
-                if(numbers[j] < numbers[k]){
-                    int temp = numbers[j];
-                    numbers[j] = numbers[k];
-                    numbers[k] = temp;
-                }
-            }
-        }
-        for(int j = 0; j < i; j++){
-            System.out.println(numbers[j]);
+       Collections.sort(numbers);
+        for(int i : numbers){
+              System.out.println(i);
         }
     }
 }
