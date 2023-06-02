6dd1ad002d94f9f9fc20689890788b1faf84ea1a
JOMAR NACORDA <jnacorda04@gmail.com>
2023-06-03 10:35:47 +1200

    e

--- exercise39.java
+++ exercise39.java
@@ -1,29 +1,4 @@
-/*
-The file gems.txt contains a list of gems in a fantasy game and their game
-value in gold pieces.
-Each line in the file contains a gem name (without spaces) and an integer
-value in gold pieces – the name and the value are separated by a colon:
-Read the list and create a HashMap using the gem name as the key and
-the value as the data
-Sample file gems.txt
-quartz:4
-pearl:6
-onyx:1
-ruby:40
-calcite:1
-From standard input read a list of adventurer's loot.
-Each line contains a series of lines containing two gem names and how
-many of each the adventurer has acquired (as an integer)
-For each line output the total value of the gems followed by gp (gold
-pieces) as in the sample output.
-The input data will end in a single line containing a #
-Do not process the line with the # in it
-Assume all gems exist in the file.
-Sample Input Sample Output
-pearl 2 ruby 1 52 gp
-onyx 5 quartz 3 17 gp
-#
-*/
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
