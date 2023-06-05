203c87407e629e844f33c397dd162f023e97ada6
JOMAR NACORDA <jnacorda04@gmail.com>
2023-06-05 23:18:18 +1200

    e

--- exercise34.java
+++ exercise34.java
@@ -5,34 +5,37 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class exercise34 {
-    public static void main(String[] args) {
-        try {
-            BufferedReader in = new BufferedReader(new FileReader("pricelist.txt"));
-            String line = in.readLine();
-            HashMap<String, Double> prices = new HashMap<String, Double>();
-            while (line != null) {
-                String[] data = line.split(" ");
-                String name = data[0];
-                double price = Double.parseDouble(data[1]);
-                prices.put(name, price);
-                line = in.readLine();
+    public static void main(String[] args) throws IOException {
+
+        HashMap<String, Double> prices = readPrice("prices.txt");
+        String line;y
+        Scanner keyboard = new Scanner(System.in);
+        line = keyboard.nextLine();
+        while (!line.equals("#")) {
+            String[] data = line.split(" ");
+            String name = data[0];
+            int quantity = Integer.parseInt(data[1]);
+            if (prices.containsKey(name)) {
+                double price = prices.get(name);
+                System.out.printf("%s %d@$%.2f $%.2f%n", name, quantity, price, price * quantity);
+            } else {
+                System.out.printf("%s Not available%n", name);
             }
-            Scanner keyboard = new Scanner(System.in);
             line = keyboard.nextLine();
-            while (!line.equals("#")) {
-                String[] data = line.split(" ");
-                String name = data[0];
-                int quantity = Integer.parseInt(data[1]);
-                if (prices.containsKey(name)) {
-                    double price = prices.get(name);
-                    System.out.printf("%s %d@$%.2f $%.2f%n", name, quantity, price, price * quantity);
-                } else {
-                    System.out.printf("%s Not available%n", name);
-                }
-                line = keyboard.nextLine();
-            }
-        } catch (IOException e) {
-            System.out.println(e);
         }
+    }
+
+    public static HashMap readPrice(String filename) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader(filename));
+        String line = in.readLine();
+        HashMap<String, Double> prices = new HashMap<String, Double>();
+        while (line != null) {
+            String[] data = line.split(" ");
+            String name = data[0];
+            double price = Double.parseDouble(data[1]);
+            prices.put(name, price);
+            line = in.readLine();
+        }
+        return prices;
     }
 }
