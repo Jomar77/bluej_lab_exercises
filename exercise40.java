cbccc8691c98741c89144f8003298fa270a669cc
JOMAR NACORDA <jnacorda04@gmail.com>
2023-06-05 13:45:31 +1200

    e

--- exercise40.java
+++ exercise40.java
@@ -6,39 +6,64 @@ import java.util.Scanner;
 
 public class exercise40 {
     public static void main(String[] args) throws IOException{
-            BufferedReader in = new BufferedReader(new FileReader("melee.txt"));
-            String line = in.readLine();
-            HashMap<String, Double> weapons = new HashMap<String, Double>();
-            while (line != null) {
-                String[] data = line.split(":");
-                String weapon = data[0];
-                double value = Double.parseDouble(data[1]);
-                weapons.put(weapon, value);
-                line = in.readLine();
-            }
+            HashMap<String, Double> weapons = meleeReader("melee.txt");
+            HashMap<String, Double> input = new HashMap<String, Double>();
             Scanner keyboard = new Scanner(System.in);
-            line = keyboard.nextLine();
+            String line = keyboard.nextLine();
             while (!line.equals("#")) {
-                String[] data = line.split(" ");
-                double totalHero = 0;
-                double totalOgre = 0;
-                for (int i = 0; i < data.length; i += 2) {
-                    String weapon = data[i];
-                    double effectiveness = Double.parseDouble(data[i + 1]);
-                    if (i < data.length / 2) {
-                        totalHero += weapons.get(weapon) * effectiveness;
-                    } else {
-                        totalOgre += weapons.get(weapon) * effectiveness;
-                    }
-                }
-                if (totalHero > totalOgre) {
-                    System.out.println("Hero");
-                } else if (totalHero < totalOgre) {
-                    System.out.println("Ogre");
-                } else {
-                    System.out.println("Neither");
-                }
+                Double attack = attackCompare(line, input);
+                input.put(line, attack);
                 line = keyboard.nextLine();
             }
+            for (String key : input.keySet()) {
+                process(key, weapons);
+            }
+    }
+    public static Double attackCompare(String line, HashMap<String, Double> distances) {
+        String[] data = line.split(",");
+        Double compare = 0.0;
+        for (int i = 0; i < data.length; i++) {
+            String from = data[i].split(":")[0];
+            String to = data[i].split(":")[1];
+            compare += distances.get(from + ":" + to);
+        }
+        return compare;
+    }
+
+    public static void process(String line, HashMap<String, Double> weapons) {
+        String[] data = line.split(" ");
+        double totalHero = 0;
+        double totalOgre = 0;
+        for (int i = 0; i < data.length; i += 2) {
+            String weapon = data[i];
+            double effectiveness = Double.parseDouble(data[i + 1]);
+            if (i < data.length / 2) {
+                totalHero += weapons.get(weapon) * effectiveness;
+            } else {
+                totalOgre += weapons.get(weapon) * effectiveness;
+            }
+        }
+        if (totalHero > totalOgre) {
+            System.out.println("Hero");
+        } else if (totalHero < totalOgre) {
+            System.out.println("Ogre");
+        } else {
+            System.out.println("Neither");
+        }
+
+    }
+
+    public static HashMap<String, Double> meleeReader(String filename) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader(filename));
+        HashMap<String, Double> weapons = new HashMap<String, Double>();
+        String line = in.readLine();
+        while (line != null) {
+            String[] data = line.split(":");
+            String weapon = data[0];
+            double value = Double.parseDouble(data[1]);
+            weapons.put(weapon, value);
+            line = in.readLine();
+        }
+        return weapons;
     }
 }
