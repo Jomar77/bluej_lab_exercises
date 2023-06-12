a7591ecd77a01a7bf13c149cc080c590be677a4e
JOMAR NACORDA <jnacorda04@gmail.com>
2023-06-12 16:42:02 +1200

    e

--- exercise42.java
+++ exercise42.java
@@ -1,28 +1,29 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class exercise42 {
-
     public static void main(String[] args) throws IOException {
-        HashMap <String,String> morseCode = readMorse("morse.txt");
+        HashMap<String, String> morseCode = readMorse("morse.txt");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("#")) {
             String[] words = input.split(" ");
             for (String word : words) {
                 for (int i = 0; i < word.length(); i++) {
-                    System.out.print(morseCode.get(word.substring(i, i + 1).toUpperCase()) + " ");
+                    if ((i == word.length() - 1)) {
+                        System.out.print(morseCode.get(word.substring(i, i + 1).toUpperCase()).strip());
+                    } else {
+                        System.out.print(morseCode.get(word.substring(i, i + 1).toUpperCase()) + " ");
+                    }
                 }
+                System.out.print(" ");
             }
             System.out.println();
             input = sc.nextLine();
         }
-        sc.close();
     }
-    public static HashMap <String,String> readMorse(String filename)  throws IOException{
+
+    public static HashMap<String, String> readMorse(String filename) throws IOException {
         HashMap<String, String> morseCode = new HashMap<String, String>();
         BufferedReader br = new BufferedReader(new FileReader(filename));
         String line;
@@ -30,7 +31,6 @@ public class exercise42 {
             String[] parts = line.split(":");
             morseCode.put(parts[0], parts[1]);
         }
-        br.close();
         return morseCode;
     }
 }
