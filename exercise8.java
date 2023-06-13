75db0dfa493ac15d002fa6e4aed324829ec0aaf3
JOMAR NACORDA <jnacorda04@gmail.com>
2023-06-14 10:27:23 +1200

    e

--- exercise8.java
+++ exercise8.java
@@ -1,7 +1,4 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
 
 
 import java.util.Scanner;
