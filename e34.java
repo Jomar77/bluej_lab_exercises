580946c5977a07a758613186614a067cf7cba5ad
JOMAR NACORDA <jnacorda04@gmail.com>
2023-06-17 12:12:06 +1200

    e

---
+++ e34.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+/**
+ * Write a description of class prac here.
+ *
+ * @Jomar
+ * @ersion (a version number or a date)
+ */
+public class e34
+{
+    public static void main (String args[]) throws IOException{
+        Scanner kb = new Scanner(System.in);
+        HashMap<String, Double> prices = readPrice("pricelist.txt");
+        String in = kb.next();
+        double num = kb.nextInt();
+        while(!in.equals("#")){
+            if(prices.containsKey(in)){
+                System.out.printf("%s %d@$@%.2f $%.2f/n",in,num,prices.get(in), num*prices.get(in));
+            }else{
+                System.out.printf("%s Not available/n",in);
+            }
+            in = kb.next();
+            num = kb.nextInt();        
+        }
+
+    }
+
+    public static HashMap<String,Double> readPrice(String filename) throws IOException{
+
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        String line = br.readLine();
+        HashMap<String,Double> pricelist = new HashMap<String,Double>();
+        while(line != null){
+            String [] data = line.split(" ");
+            String name = data[0];
+            Double price = Double.parseDouble(data[1]);
+            pricelist.put(name,price);            
+        }
+
+        return pricelist;
+    }
+}
