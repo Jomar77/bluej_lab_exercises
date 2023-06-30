d7b1fb65433394684e6a3dc42511fb7acb03fa82
JOMAR NACORDA <jnacorda04@gmail.com>
2023-06-30 17:40:01 +1200

    done

--- exam.java
+++ exam.java
@@ -1,10 +1,7 @@
-/*
- * provide a series of methods for code reading exam
- * 
- */
-
-
 public class exam {
-   
-    
+    public static void main(String[] args) {
+        int a = 14;
+        int num = a >> 1 ;
+        System.out.println(num);
+    }
 }
