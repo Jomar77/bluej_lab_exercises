b684211004bf6c31b04dc7c5b26e2b95083ad379
JOMAR NACORDA <jnacorda04@gmail.com>
2023-08-30 21:54:05 +1200

    e

--- e17gab.java
+++ e17gab.java
@@ -1,41 +1,35 @@
 import java.util.Scanner;
 
-public class exercise22 {
+public class e17gab {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String line = ""; 
+        String line = "";
+        String firstname = "";
+        String surname = "";
+        int total = 0;
+        int score = 0;
 
         line = scan.nextLine();
         while(!line.equals("#"))
         {
             Scanner scanLine = new Scanner(line);
             scanLine.useDelimiter(",");
-            System.out.println(Name(scanLine) + " " + total(scanLine));
+            surname = scanLine.next();
+            firstname = scanLine.next();
+            System.out.println(firstname + " " + surname + " " + total(scanLine));
+            total = 0;
             line = scan.nextLine();
         }
     }
-    
+
     public static int total(Scanner scanLine) {
         int total = 0;
         int score = 0;
         while(scanLine.hasNextInt())
-        {
+            {
                 score = scanLine.nextInt();
                 total = total + score;
             }
         return total;
-    }
-
-    public static String Name(Scanner scanLine){
-        String firstname = "";
-        String surname = "";
-        String name = "";
-
-            surname = scanLine.next();
-            firstname = scanLine.next();
-
-            name =  surname + " " + firstname;
-        
-        return name;
     }
 }
